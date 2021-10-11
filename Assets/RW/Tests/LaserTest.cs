d950e64293a4e211ee92a5574f909eb95e906df4
C12345678 <martin.harrigan@itcarlow.ie>
2021-10-11 11:33:32 +0100

    Test passing

---
+++ Assets/RW/Tests/LaserTest.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.TestTools;
+using NUnit.Framework;
+using System.Collections;
+
+public class LaserTest
+{
+    [UnityTest]
+    public IEnumerator LaserDestroysAsteroid()
+    {
+        GameObject gameGameObject =
+        MonoBehaviour.Instantiate(Resources.Load<GameObject>("Prefabs/Game"));
+        Game game = gameGameObject.GetComponent<Game>();
+        GameObject asteroid = game.GetSpawner().SpawnAsteroid();
+        asteroid.transform.position = Vector3.zero;
+        GameObject laser = game.GetShip().SpawnLaser();
+        laser.transform.position = Vector3.zero;
+        yield return new WaitForSeconds(0.1f);
+        UnityEngine.Assertions.Assert.IsNull(asteroid);
+        Object.Destroy(game.gameObject);
+    }
+}
+
