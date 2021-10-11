2e74ba0279f315f37db5741284b7861d5a4a2e3a
C12345678 <martin.harrigan@itcarlow.ie>
2021-10-11 11:33:32 +0100

    Task 1.1 finished: implementation and test code (JIRA 123)

---
+++ Assets/RW/Tests/LaserTest.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using UnityEngine.TestTools;
+using NUnit.Framework;
+using System.Collections;
+
+public class LaserTest
+{
+
+    private Game game;
+
+    [SetUp]
+    public void Setup()
+    {
+        GameObject gameGameObject =
+          MonoBehaviour.Instantiate(
+            Resources.Load<GameObject>("Prefabs/Game"));
+        game = gameGameObject.GetComponent<Game>();
+    }
+
+    [TearDown]
+    public void Teardown()
+    {
+        Object.Destroy(game.gameObject);
+    }
+
+    [UnityTest]
+    public IEnumerator LaserDestroysAsteroid()
+    {
+        GameObject asteroid = game.GetSpawner().SpawnAsteroid();
+        asteroid.transform.position = Vector3.zero;
+        GameObject laser = game.GetShip().SpawnLaser();
+        laser.transform.position = Vector3.zero;
+        yield return new WaitForSeconds(0.1f);
+        UnityEngine.Assertions.Assert.IsNull(asteroid);
+    }
+}
